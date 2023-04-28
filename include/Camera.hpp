37480f526f4e636e976d00bbb9c56f4c7b45fedc
Rukiye Devran <ruki.dev@gmail.com>
2023-04-28 22:34:44 +0200

    add quaternion camera

--- include/Camera.hpp
+++ include/Camera.hpp
@@ -3,6 +3,7 @@
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/mat4x4.hpp>
 #include <glm/vec3.hpp>
+#include <glm/gtc/quaternion.hpp>
 
 class Camera
 {
@@ -12,25 +13,23 @@ public:
     glm::mat4 view;
 
     Camera(float fov, float width, float height);
-    const glm::mat4& getVP() const;
+    const glm::mat4 getVP();
     void translate(glm::vec3 v);
     void onMouseMove(float xRel, float yRel);
     void moveFront(float amount);
     void moveRight(float amount);
     void moveDown(float amount);
+    void rotate(float amount);
     void updateScreenSize(float width, float height);
     const glm::vec3& getPosition() const;
     float getNear() const;
     float getFar() const;
 
 private:
-    glm::mat4 vp;
-    glm::vec3 up;
+    glm::quat orientation;
     glm::vec3 u, v, w;
     float near, far;
-    float yaw, pitch;
+    float yaw, pitch, roll;
     float fov;
     const float mouse_sensitivity = 0.25f;
-
-    void update();
 };
