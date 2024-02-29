1a827f31b03b36a6d49cb89d5fa7f2dbd36361d8
Matthias Bruns <matthiasbruns35@gmail.com>
2024-02-29 22:20:47 +0100

    add cl options for training and loading of the nn

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,8 @@
+#include <boost/program_options/options_description.hpp>
+#include <boost/program_options/value_semantic.hpp>
+#include <boost/program_options/variables_map.hpp>
 #include <iostream>
-#include <filesystem>
-#include <thread>
+#include <boost/program_options.hpp>
 #define GLM_FORCE_RADIANS
 #define GLM_FORCE_DEPTH_ZERO_TO_ONE
 #include <glm/mat4x4.hpp>
@@ -14,7 +16,6 @@
 #include "vk/Timer.hpp"
 #include "vk/VulkanMainContext.hpp"
 #include "vk/VulkanCommandContext.hpp"
-#include "Storage.hpp"
 #include "WorkContext.hpp"
 #include "Agent.hpp"
 
@@ -318,8 +319,36 @@ private:
     }
 };
 
+int parse_args(int argc, char** argv, boost::program_options::variables_map& vm)
+{
+    namespace bpo = boost::program_options;
+    bpo::options_description desc("Allowed options");
+    desc.add_options()
+        ("help", "Print this help message")
+        ("nn_file", bpo::value<std::string>(), "Load neural network checkpoint from given file and initialize the agent with this network")
+        ("train_mode,T", "Train agent")
+        ("disable_rendering,R", "Do not render the game to enable quicker training iterations")
+    ;
+    bpo::store(bpo::parse_command_line(argc, argv, desc), vm);
+    bpo::notify(vm);
+    if (vm.count("help"))
+    {
+        std::cout << desc << std::endl;
+        return 1;
+    }
+    return 0;
+}
+
 int main(int argc, char** argv)
 {
+    boost::program_options::variables_map vm;
+    int parse_status = parse_args(argc, argv, vm);
+    if (parse_status != 0) return parse_status;
+    std::string nn_file = "";
+    if (vm.count("nn_file")) nn_file = vm["nn_file"].as<std::string>();
+    bool train_mode = vm.count("train_mode");
+    bool disable_rendering = vm.count("disable_rendering");
+
     std::vector<spdlog::sink_ptr> sinks;
     sinks.push_back(std::make_shared<spdlog::sinks::stdout_sink_st>());
     sinks.push_back(std::make_shared<spdlog::sinks::basic_file_sink_st>("ve.log", true));
@@ -329,7 +358,7 @@ int main(int argc, char** argv)
     spdlog::set_pattern("[%Y-%m-%d %T.%e] [%L] %v");
     spdlog::info("Starting");
     auto t1 = std::chrono::high_resolution_clock::now();
-    MainContext mc;
+    MainContext mc(nn_file, train_mode, disable_rendering);
     auto t2 = std::chrono::high_resolution_clock::now();
     spdlog::info("Setup took: {} ms", (std::chrono::duration<double, std::milli>(t2 - t1).count()));
     mc.run();
